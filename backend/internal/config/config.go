1eeaaeea746f54998bfee74be8f0491776c742fe
wrong89 <fenomen_03@mail.ru>
2025-04-14 15:43:04 +0300

    Added config

---
+++ backend/internal/config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/joho/godotenv/autoload"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	HTTPServer  HTTPServer `yaml:"http_server"`
+	Env         string     `yaml:"env" env-default:"local"`
+	StoragePath string     `yaml:"storage_path" env-required:"true"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-default:"localhost:5000"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is empty")
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file not found: %s", configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("cannot read config: %s", err)
+	}
+
+	return &cfg
+}
