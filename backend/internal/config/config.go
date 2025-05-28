7e6a666e0a70c21527e36f9e93834f644afe85e1
wrong89 <fenomen_03@mail.ru>
2025-05-28 23:28:22 +0300

    config changed

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -11,9 +11,8 @@ import (
 )
 
 type Config struct {
-	HTTPServer  HTTPServer `yaml:"http_server"`
-	Env         string     `yaml:"env" env-default:"local"`
-	StoragePath string     `yaml:"storage_path" env-required:"true"`
+	HTTPServer HTTPServer `yaml:"http_server"`
+	Env        string     `yaml:"env" env-default:"local"`
 }
 
 type HTTPServer struct {
